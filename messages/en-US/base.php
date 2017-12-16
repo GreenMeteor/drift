6a03eada0d55565072800f9bac90419be87aafef
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-12-16 20:45:38 +0900

    Delete base.php

--- messages/en-US/base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	return [
-		'<strong>LinkedIn</strong>' => '<strong>LinkedIn</strong>',
-		'LinkedIn Settings' => 'LinkedIn Settings',
-		'LinkedIn Widget URL:' => 'LinkedIn Widget URL:',
-		'<strong>LinkedIn</strong> module configuration' => '<strong>LinkedIn</strong> module configuration',
-		'Save' => 'Save',
-	];
