c31fafe12f800cdebf15c1b7084e2e58325f7e39
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-10-06 23:59:00 -0700

    Chg: ::className() is deprecated + PHP 7.2 support

--- Events.php
+++ Events.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
 use humhub\modules\drift\widgets\DriftFrame;
 use humhub\models\Setting;
 
-class Events extends \yii\base\Object
+class Events extends \yii\base\BaseObject
 {
 
     public static function onAdminMenuInit(\yii\base\Event $event)
@@ -19,8 +19,8 @@ public static function addDriftFrame($event)
         if (Yii::$app->user->isGuest) {
             return;
         }
-        $event->sender->view->registerAssetBundle(Assets::className());
-        $event->sender->addWidget(DriftFrame::className(), [], [
+        $event->sender->view->registerAssetBundle(Assets::class);
+        $event->sender->addWidget(DriftFrame::class, [], [
             'sortOrder' => Setting::Get('timeout', 'drift')
         ]);
     }
