262a9fe76c67a43ef6a30188c980efc0dd7e999f
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-07-15 00:48:12 -0400

    Enh: Create backend support for jsCode model

--- widgets/DriftFrame.php
+++ widgets/DriftFrame.php
@@ -3,23 +3,21 @@
 namespace humhub\modules\drift\widgets;
 
 use Yii;
-use yii\helpers\Url;
-use humhub\libs\Html;
 use humhub\components\Widget;
 
 /**
+ * DriftFrame adds snippet code to all layouts extended by config.php
  *
  * @author Felli
  */
 class DriftFrame extends Widget
 {
-    public $contentContainer;
-
     /**
      * @inheritdoc
      */
-   public function run()
+    public function run()
     {
-        return $this->render('driftframe');
+        return Yii::$app->settings->get('jsCode');
+        return $this->render('driftframe', []);
     }
 }
