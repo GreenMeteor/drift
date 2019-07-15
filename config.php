e8265011aee838f7dec27add5daa89abf0c46075
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-07-15 00:42:09 -0400

    Chg: Refactoring

--- config.php
+++ config.php
@@ -2,34 +2,17 @@
 
 namespace humhub\modules\drift;
 
+use humhub\modules\dashboard\widgets\Sidebar;
+use humhub\modules\admin\widgets\AdminMenu;
+
 return [
     'id' => 'drift',
     'class' => 'humhub\modules\drift\Module',
     'namespace' => 'humhub\modules\drift',
     'events' => [
-        [
-            'class' => \humhub\modules\dashboard\widgets\Sidebar::class,
-            'event' => \humhub\modules\dashboard\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\drift\Events',
-                'addDriftFrame'
-            ]
+        ['class' => Sidebar::class, 'event' => Sidebar::EVENT_INIT, 'callback' => ['humhub\modules\drift\Events', 'addDriftFrame']
         ],
-        [
-            'class' => \humhub\modules\space\widgets\Sidebar::class,
-            'event' => \humhub\modules\space\widgets\Sidebar::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\drift\Events',
-                'addDriftFrame'
-            ]
-        ],
-        [
-            'class' => \humhub\modules\admin\widgets\AdminMenu::class,
-            'event' => \humhub\modules\admin\widgets\AdminMenu::EVENT_INIT,
-            'callback' => [
-                'humhub\modules\drift\Events',
-                'onAdminMenuInit'
-            ]
+        ['class' => AdminMenu::class, 'event' => AdminMenu::EVENT_INIT, 'callback' => ['humhub\modules\drift\Events', 'onAdminMenuInit']
         ]
     ]
 ];
