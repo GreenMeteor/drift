91644e546659c36a92b4468549803e42b39ec52f
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-07-15 00:49:33 -0400

    Enh: Add codebox to use from admin settings

--- widgets/views/driftframe.php
+++ widgets/views/driftframe.php
@@ -1,35 +1,12 @@
 <?php
 
-use yii\helpers\Url;
-use humhub\libs\Html;
-use humhub\models\Setting;
+use yii\widgets\ActiveForm;
 
 \humhub\modules\drift\Assets::register($this);
 ?>
 
-<?= Html::beginTag('div') ?>
-
-<script type="text/javascript">
-!function() {
-  var t;
-  if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
-  t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-  t.factory = function(e) {
-    return function() {
-      var n;
-      return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
-    };
-  }, t.methods.forEach(function(e) {
-    t[e] = t.factory(e);
-  }), t.load = function(t) {
-    var e, n, o, i;
-    e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
-    o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
-    n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
-  });
-}();
-drift.SNIPPET_VERSION = '0.3.1';
-drift.load('ig4sfzx6rufy');
-</script>
-
-<?= Html::endTag('div'); ?>
+<?php $form = ActiveForm::begin(['id' => 'configure-form']); ?>
+    <div class="form-group">
+        <?= $form->field($model, 'jsCode'); ?>
+    </div>
+<?php ActiveForm::end(); ?>
